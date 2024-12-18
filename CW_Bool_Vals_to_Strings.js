804b868aac817fdc8f078443a0c956e3b996864c
Xavier <xavier.f.nealcarson@gmail.com>
2024-12-17 20:15:31 -0600

    Add files via upload

---
+++ CW_Bool_Vals_to_Strings.js
@@ -0,0 +1,11 @@
+//INSTIRUCTIONS//
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//MY ANSWER//
+function boolToWord( bool ){
+    if(bool == true){
+      return "Yes";
+    }else if(bool == false){
+      return "No";
+    };
+  }
