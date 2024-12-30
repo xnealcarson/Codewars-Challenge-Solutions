a8d51faac55ff5f7decd64fa0b6bfb495b1817b9
Xavier <xavier.f.nealcarson@gmail.com>
2024-12-29 23:13:05 -0600

    Add files via upload

---
+++ CW_invertVals.js
@@ -0,0 +1,14 @@
+//INSTRUCTIONS//
+/*Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+[1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+[1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+[] --> []
+You can assume that all values are integers. Do not mutate the input array.*/
+
+
+//MY ANSWER//
+function invert(array) {
+   const invertArr = array.map (value => -value)
+   return invertArr
+}
