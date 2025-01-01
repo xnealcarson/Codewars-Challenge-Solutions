ee0a27116c68af4c896641887a88be8d8bc91138
Xavier <xavier.f.nealcarson@gmail.com>
2024-12-31 22:26:54 -0600

    Add files via upload

---
+++ CW_functionSyntaxDebug.js
@@ -0,0 +1,13 @@
+//INSTRUCTIONS//
+/*Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+Student's code:
+function main [verb, noun]
+  return verb + noun
+}*/
+
+//MY ANSWER//
+function main (verb, noun) {
+    return verb + noun
+  }
